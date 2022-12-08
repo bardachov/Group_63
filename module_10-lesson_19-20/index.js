d1499659203a154cef828c64f3919d5da8c8d8a1
Artem Bardachov <artem_bardachov@epam.com>
2022-12-08 21:53:04 +0200

    fix: Refactored pagination logic

--- module_10-lesson_19-20/index.js
+++ module_10-lesson_19-20/index.js
@@ -40,41 +40,35 @@ const searchField = document.getElementById('searchNewsField'); // поле вв
 const articlesContainer = document.getElementById('articles'); // контейнер для статей
 const paginationContainer = document.getElementById('pagination'); // контейнер для пагінації
 
+const API = axios.create({
+  baseURL: 'https://newsapi.org/v2/everything',
+  headers: {
+    "X-Api-Key": API_KEY
+  }
+})
 
-searchBtnRef.addEventListener('click', e => {
-  
-  getNews(searchField.value).then(({articles, totalResults}) => {
-    calculatePagination(totalResults)
+function handleArticles({articles, totalResults}) {
+  totalPages = calculatePagination(totalResults)
     
-    const elements = createArticlesElements(articles);
-    const pageElements = createPagesElements();
-    
-    render(paginationContainer, pageElements);
-    render(articlesContainer, elements);
-  });
+  const elements = createArticlesElements(articles);
+  const pageElements = createPagesElements();
   
-});
+  render(paginationContainer, pageElements);
+  render(articlesContainer, elements);
+}
+
+searchBtnRef.addEventListener('click', e => getNews(searchField.value).then(handleArticles));
 
 paginationContainer.addEventListener('click', e => {
   e.preventDefault();
   if (e.target.className !== "page-link") return
 
   currentPage = e.target.dataset.page;
-
-  getNews(searchField.value).then(({articles, totalResults}) => {
-    calculatePagination(totalResults)
-    
-    const elements = createArticlesElements(articles);
-    const pageElements = createPagesElements();
-    
-    render(paginationContainer, pageElements);
-    render(articlesContainer, elements);
-  });
-
+  getNews(searchField.value).then(handleArticles)
 })
 
 loadMoreBtnRef.addEventListener('click', e => {
-  if (currentPage > totalPages) return
+  if (currentPage >= totalPages) return
   
   currentPage = Number(currentPage) + 1;
   
@@ -89,38 +83,40 @@ loadMoreBtnRef.addEventListener('click', e => {
  * @param {String} query рядок з ключовими словами для пошуку
  * @returns {Promise} проміс, успішне відпрацювання якого повертає масив з новинами і загальну кількість новин по запиту.
  */
-function getNews(query) {
-  const urlAPI = `https://newsapi.org/v2/everything?q=${query}&from=2022-12-01&apiKey=${API_KEY}&pageSize=${PageSize}&page=${currentPage}`;
-  
-  return axios.get(urlAPI)
+const getNews = (query) => API.get('', {
+    params: {
+      from: '2022-12-01',
+      pageSize: PageSize,
+      page: currentPage,
+      q: query
+    }
+  })
   .then(res => res.data)
   .then(({articles, totalResults}) => {
     return {articles, totalResults}
   })
   .catch(error => console.log(error))
-}
+
 
 /**
  * createArticlesElements
  * @param {Array} articles - масив з новинами
  * @returns {String} Рядок з елементами всіх новин
  */
-function createArticlesElements (articles) {
-  return articles.map(({title, description, url, urlToImage}) => {
+const createArticlesElements = articles => articles.map(({title, description, url, urlToImage}) => {
     return `<article class="news">
     <h2>${title}</h2>
     <div>${description}</div>
     <img src="${urlToImage}" alt="${title}" loading="lazy">
     <a href="${url}" class="news-link" target="_blank">more</a>
   </article>`;
-  }).join('');
-}
+  }).join('')
 
 /**
  * createPagesElements
  * @returns {string} Рядок з елементами кнопок пагінації
  */
-function createPagesElements () {
+const createPagesElements = () => {
   let pagesElements = '';
   
   for (let i = 1; i <= totalPages; i += 1 ) {
@@ -134,16 +130,14 @@ function createPagesElements () {
  * calculatePagination. Підраховує кількість сторінок
  * @param {Number} totalResults 
  */
-function calculatePagination(totalResults) {
-  totalPages = Math.floor((totalResults > 100 ? 100 : totalResults) / PageSize)
-}
+const calculatePagination = totalResults => Math.floor((totalResults > 100 ? 100 : totalResults) / PageSize)
 
 /**
  * Очищає контейнер і вставляє туди новий контент
  * @param {HTMLElement} container 
  * @param {String} content 
  */
-function render (container, content) {
+const render = (container, content) => {
   container.innerHTML = '';
   container.insertAdjacentHTML('beforeend', content)
 }
